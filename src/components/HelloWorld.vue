fc91c804d98f6b43ac35b6014f674bee3bce9da4
Joyceandlee <2473011861@qq.com>
2019-07-08 09:09:26 +0800

    提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+   <h3>HelloWorld</h3>
+   <div>
+     home页面
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+h3{
+  margin: 0;padding: 0;
+  color: #fcf;
+  text-align: center;
+}
+</style>
+
+
+
